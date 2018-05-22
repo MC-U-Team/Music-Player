7ab4769e30f52081fc3d3157d51ab4b9d5da95bd
HyCraftHD <hycrafthd@live.de>
2018-05-22 16:52:16 +0200

    Removed version replacement as it is not needed

--- src/main/java/info/u_team/music_player/MusicPlayerConstants.java
+++ src/main/java/info/u_team/music_player/MusicPlayerConstants.java
@@ -10,9 +10,9 @@ public class MusicPlayerConstants {
 	
 	public static final String MODID = "musicplayer";
 	public static final String NAME = "Music Player";
-	public static final String VERSION = "@VERSION@";
+	public static final String VERSION = "1.0.0";
 	public static final String MCVERSION = "[1.8,1.12.2]";
-	public static final String DEPENDENCIES = "required-after:uteamcore@[@UTEAMCOREVERSION@,);";
+	public static final String DEPENDENCIES = "required-after:uteamcore@[1.1.1,);";
 	public static final String UPDATEURL = "https://api.u-team.info/update/musicplayer.json";
 	
 	public static final Logger LOGGER = LogManager.getLogger(NAME);
