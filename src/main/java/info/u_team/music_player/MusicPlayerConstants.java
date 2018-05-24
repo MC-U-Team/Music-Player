ec1b3380171b1d7d1d02810ad7e3d686a9682f25
HyCraftHD <hycrafthd@live.de>
2018-05-24 16:52:57 +0200

    Commit forced, because of new build

---
+++ src/main/java/info/u_team/music_player/MusicPlayerConstants.java
@@ -0,0 +1,27 @@
+package info.u_team.music_player;
+
+import java.io.File;
+
+import org.apache.logging.log4j.*;
+
+import info.u_team.music_player.classloader.CustomURLClassLoader;
+
+public class MusicPlayerConstants {
+	
+	public static final String MODID = "musicplayer";
+	public static final String NAME = "Music Player";
+	public static final String VERSION = "1.0.0";
+	public static final String MCVERSION = "[1.8,1.12.2]";
+	public static final String DEPENDENCIES = "required-after:uteamcore@[1.1.1,);";
+	public static final String UPDATEURL = "https://api.u-team.info/update/musicplayer.json";
+	
+	public static final Logger LOGGER = LogManager.getLogger(NAME);
+	
+	public static final File PATH = new File(new File(System.getProperty("user.dir")), "musicplayer");
+	
+	public static final CustomURLClassLoader CLASSLOADER = new CustomURLClassLoader();
+	
+	private MusicPlayerConstants() {
+	}
+	
+}
