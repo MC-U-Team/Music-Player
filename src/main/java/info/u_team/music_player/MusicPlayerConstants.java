98d320cc75bf5975d68875b14b8614b07a2c9666
HyCraftHD <hycrafthd@live.de>
2018-05-24 22:05:23 +0200

    Added @VERSION@ and used every build task to update.

--- src/main/java/info/u_team/music_player/MusicPlayerConstants.java
+++ src/main/java/info/u_team/music_player/MusicPlayerConstants.java
@@ -10,7 +10,7 @@ public class MusicPlayerConstants {
 	
 	public static final String MODID = "musicplayer";
 	public static final String NAME = "Music Player";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "@VERSION@";
 	public static final String MCVERSION = "[1.8,1.12.2]";
 	public static final String DEPENDENCIES = "required-after:uteamcore@[1.1.1,);";
 	public static final String UPDATEURL = "https://api.u-team.info/update/musicplayer.json";
