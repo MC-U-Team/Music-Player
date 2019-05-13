c37ea12de111eca402ec6e701fd6e9e8594ac945
HyCraftHD <hycrafthd@live.de>
2019-05-14 01:03:08 +0200

    Removed unused font + updated resources

--- src/main/java/info/u_team/music_player/init/MusicPlayerResources.java
+++ src/main/java/info/u_team/music_player/init/MusicPlayerResources.java
@@ -5,23 +5,27 @@ import net.minecraft.util.ResourceLocation;
 
 public class MusicPlayerResources {
 	
-	public static final ResourceLocation textureCreate = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/create.png");
-	public static final ResourceLocation textureOpen = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/open.png");
-	public static final ResourceLocation textureClear = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/clear.png");
-	public static final ResourceLocation texturePlay = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/play.png");
-	public static final ResourceLocation textureStop = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/stop.png");
-	public static final ResourceLocation texturePause = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/pause.png");
-	public static final ResourceLocation textureAdd = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/add.png");
-	public static final ResourceLocation textureUp = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/up.png");
-	public static final ResourceLocation textureDown = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/down.png");
-	public static final ResourceLocation textureBack = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/back.png");
-	public static final ResourceLocation textureSkipForward = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/skip-forward.png");
-	public static final ResourceLocation textureSkipBack = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/skip-back.png");
-	public static final ResourceLocation textureShuffle = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/shuffle.png");
-	public static final ResourceLocation textureRepeat = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/repeat.png");
-	public static final ResourceLocation textureRepeatSingle = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/repeat-single.png");
+	public static final ResourceLocation textureCreate = createResource("create");
+	public static final ResourceLocation textureOpen = createResource("open");
+	public static final ResourceLocation textureClear = createResource("clear");
+	public static final ResourceLocation texturePlay = createResource("play");
+	public static final ResourceLocation textureStop = createResource("stop");
+	public static final ResourceLocation texturePause = createResource("pause");
+	public static final ResourceLocation textureAdd = createResource("add");
+	public static final ResourceLocation textureUp = createResource("up");
+	public static final ResourceLocation textureDown = createResource("down");
+	public static final ResourceLocation textureBack = createResource("back");
+	public static final ResourceLocation textureSkipForward = createResource("skip-forward");
+	public static final ResourceLocation textureSkipBack = createResource("skip-back");
+	public static final ResourceLocation textureShuffle = createResource("shuffle");
+	public static final ResourceLocation textureRepeat = createResource("repeat");
+	public static final ResourceLocation textureRepeatSingle = createResource("repeat-single");
 	
-	public static final ResourceLocation textureSocialYoutube = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/socialmedia/youtube-logo.png");
-	public static final ResourceLocation textureSocialSoundcloud = new ResourceLocation(MusicPlayerMod.modid, "textures/gui/socialmedia/soundcloud-logo.png");
+	public static final ResourceLocation textureSocialYoutube = createResource("socialmedia/youtube-logo");
+	public static final ResourceLocation textureSocialSoundcloud = createResource("socialmedia/soundcloud-logo");
+	
+	private static ResourceLocation createResource(String name) {
+		return new ResourceLocation(MusicPlayerMod.modid, "textures/gui/" + name + ".png");
+	}
 	
 }
--- src/main/java/info/u_team/music_player/proxy/ClientProxy.java
+++ src/main/java/info/u_team/music_player/proxy/ClientProxy.java
@@ -4,8 +4,6 @@ import info.u_team.music_player.dependency.DependencyManager;
 import info.u_team.music_player.event.EventHandlerMusicPlayer;
 import info.u_team.music_player.init.*;
 import info.u_team.music_player.musicplayer.MusicPlayerManager;
-import info.u_team.music_player.musicplayer.playlist.Playlist;
-import info.u_team.music_player.util.WrappedObject;
 import info.u_team.u_team_core.api.IModProxy;
 import info.u_team.u_team_core.registry.util.CommonRegistry;
 import net.minecraftforge.api.distmarker.*;
@@ -27,18 +25,10 @@ public class ClientProxy extends CommonProxy implements IModProxy {
 	public void setup() {
 		super.setup();
 		CommonRegistry.registerEventHandler(EventHandlerMusicPlayer.class);
-		
-		Playlist playlist = new Playlist("GENERATED PLAYLIST");
-		playlist.uris.add(new WrappedObject<>("https://www.youtube.com/watch?v=Rj0piP6FJec"));
-		playlist.uris.add(new WrappedObject<>("https://www.youtube.com/playlist?list=PLyseegEZ84-drkYTkLldkiIBPHRJd7Xgd"));
-		MusicPlayerManager.getPlaylistManager().getPlaylists().add(playlist);
-		System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
 	}
 	
 	@Override
 	public void complete() {
 		super.complete();
-		System.out.println("____________________WHY IS THIS NOT CALLED EVERYTIME???___________________________");
-		MusicPlayerFonts.setup();
 	}
 }
