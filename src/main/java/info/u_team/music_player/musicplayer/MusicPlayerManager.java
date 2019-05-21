3bfbc5d2c6709ea4fb445624cb93d58f67fbfe29
HyCraftHD <hycrafthd@live.de>
2019-05-21 22:49:35 +0200

    Now using FmlCommonHandler to exit java if something failes

--- src/main/java/info/u_team/music_player/musicplayer/MusicPlayerManager.java
+++ src/main/java/info/u_team/music_player/musicplayer/MusicPlayerManager.java
@@ -6,27 +6,28 @@ import com.google.gson.*;
 
 import info.u_team.music_player.dependency.DependencyManager;
 import info.u_team.music_player.lavaplayer.api.IMusicPlayer;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class MusicPlayerManager {
-
+	
 	private static final Logger logger = LogManager.getLogger();
-
+	
 	private static IMusicPlayer player;
-
+	
 	private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
+	
 	private static final PlaylistManager playlistManager = new PlaylistManager(gson);
 	private static final SettingsManager settingsManager = new SettingsManager(gson);
-
+	
 	public static void setup() {
 		generatePlayer();
 		player.startAudioOutput();
 		playlistManager.loadFromFile();
 		settingsManager.loadFromFile();
-
+		
 		player.setVolume(settingsManager.getSettings().getVolume());
 	}
-
+	
 	private static void generatePlayer() {
 		try {
 			Class<?> clazz = Class.forName("info.u_team.music_player.lavaplayer.MusicPlayer", true, DependencyManager.getClassLoader());
@@ -37,18 +38,18 @@ public class MusicPlayerManager {
 			logger.info("Successfully created music player instance");
 		} catch (Exception ex) {
 			logger.fatal("Cannot create music player instance. This is a serious bug and the mod will not work. Report to the mod authors", ex);
-			System.exit(0);
+			FMLCommonHandler.instance().exitJava(0, false);
 		}
 	}
-
+	
 	public static IMusicPlayer getPlayer() {
 		return player;
 	}
-
+	
 	public static PlaylistManager getPlaylistManager() {
 		return playlistManager;
 	}
-
+	
 	public static SettingsManager getSettingsManager() {
 		return settingsManager;
 	}
