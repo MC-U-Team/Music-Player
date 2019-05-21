d81e43c72236a54fbdfc4f6764305c55a551cc04
HyCraftHD <hycrafthd@live.de>
2019-05-22 00:09:06 +0200

    Remove invalid character

--- src/main/java/org/lwjgl/util/tinyfd/TinyFileDialogs.java
+++ src/main/java/org/lwjgl/util/tinyfd/TinyFileDialogs.java
@@ -11,7 +11,7 @@ import com.sun.jna.*;
  * must be installed. This class is very simple and dont offer all functions as the original. All right belongs to lwjgl
  * and the author of tinyfd.
  * 
- * Copyright © 2012-present Lightweight Java Game Library All rights reserved.
+ * Copyright (c) 2012-present Lightweight Java Game Library All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
  * following conditions are met:
