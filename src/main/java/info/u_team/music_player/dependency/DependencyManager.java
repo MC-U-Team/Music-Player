ff8bfecf871917c2e0b830951c76a7abd099cb3f
HyCraftHD <hycrafthd@live.de>
2019-06-28 11:15:37 +0200

    Fix missing try with resource statement when using Files.walk or Files.list

--- src/main/java/info/u_team/music_player/dependency/DependencyManager.java
+++ src/main/java/info/u_team/music_player/dependency/DependencyManager.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Method;
 import java.net.*;
 import java.nio.file.*;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 import org.apache.logging.log4j.*;
 
@@ -14,48 +15,48 @@ import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.moddiscovery.ModFile;
 
 public class DependencyManager {
-	
+
 	private static final Logger logger = LogManager.getLogger();
 	private static final Marker load = MarkerManager.getMarker("Load");
-	
+
 	private static final DependencyClassLoader musicplayerclassloader = new DependencyClassLoader();
-	
+
 	public static void construct() {
 		logger.info(load, "Load dependencies");
-		
+
 		final String devPath = System.getProperty("musicplayer.dev");
 		if (devPath != null) {
-			getJarFilesInDev(Paths.get(devPath, "musicplayer-lavaplayer/build/libs"), DependencyManager::addToMusicPlayerDependencies);
-			getJarFilesInDev(Paths.get(devPath, "musicplayer-lavaplayer/build/dependencies"), DependencyManager::addToMusicPlayerDependencies);
+			findJarFilesInDev(Paths.get(devPath, "musicplayer-lavaplayer/build/libs"), DependencyManager::addToMusicPlayerDependencies);
+			findJarFilesInDev(Paths.get(devPath, "musicplayer-lavaplayer/build/dependencies"), DependencyManager::addToMusicPlayerDependencies);
 		} else {
-			getJarFilesInJar("dependencies/internal", path -> addToInternalDependencies(createInternalURL(path)));
-			getJarFilesInJar("dependencies/musicplayer", path -> addToMusicPlayerDependencies(createInternalURL(path)));
+			findJarFilesInJar("dependencies/internal", path -> addToInternalDependencies(createInternalURL(path)));
+			findJarFilesInJar("dependencies/musicplayer", path -> addToMusicPlayerDependencies(createInternalURL(path)));
 		}
-		
+
 		logger.info(load, "Finished loading dependencies");
 	}
-	
+
 	public static DependencyClassLoader getClassLoader() {
 		return musicplayerclassloader;
 	}
-	
-	private static void getJarFilesInDev(Path path, Consumer<Path> consumer) {
-		try {
-			Files.walk(path).filter(file -> file.toString().endsWith(".jar")).forEach(consumer);
+
+	private static void findJarFilesInDev(Path path, Consumer<Path> consumer) {
+		try (Stream<Path> stream = Files.walk(path)) {
+			stream.filter(file -> file.toString().endsWith(".jar")).forEach(consumer);
 		} catch (IOException ex) {
 			logger.error(load, "When searching for jar files in dev an exception occured.", ex);
 		}
 	}
-	
-	private static void getJarFilesInJar(String folder, Consumer<Path> consumer) {
+
+	private static void findJarFilesInJar(String folder, Consumer<Path> consumer) {
 		final ModFile modfile = ModList.get().getModFileById(MusicPlayerMod.modid).getFile();
-		try {
-			Files.walk(modfile.findResource("/" + folder)).filter(file -> file.toString().endsWith(".jar")).forEach(consumer);
+		try (Stream<Path> stream = Files.walk(modfile.findResource("/" + folder))) {
+			stream.filter(file -> file.toString().endsWith(".jar")).forEach(consumer);
 		} catch (IOException ex) {
 			logger.error(load, "When searching for jar files in jar an exception occured.", ex);
 		}
 	}
-	
+
 	private static URL createInternalURL(Path path) {
 		final String url = "modjar://" + MusicPlayerMod.modid + path;
 		logger.debug(load, "Load url" + url);
@@ -66,17 +67,17 @@ public class DependencyManager {
 		}
 		return null;
 	}
-	
+
 	// Add to different classloader
-	
+
 	private static void addToMusicPlayerDependencies(URL url) {
 		musicplayerclassloader.addURL(url);
 	}
-	
+
 	private static void addToMusicPlayerDependencies(Path path) {
 		musicplayerclassloader.addPath(path);
 	}
-	
+
 	private static void addToInternalDependencies(URL url) {
 		try {
 			final URLClassLoader systemloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
