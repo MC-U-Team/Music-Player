e2eabef9b43fbba5890491d33b42d55553d20e25
HyCraftHD <hycrafthd@live.de>
2020-02-16 13:38:34 +0100

    Populate class

--- src/main/java/info/u_team/music_player/data/MusicPlayerDataGenerator.java
+++ src/main/java/info/u_team/music_player/data/MusicPlayerDataGenerator.java
@@ -1,6 +1,22 @@
 package info.u_team.music_player.data;
 
+import info.u_team.music_player.MusicPlayerMod;
+import info.u_team.u_team_core.data.GenerationData;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 
+@EventBusSubscriber(modid = MusicPlayerMod.MODID, bus = Bus.MOD)
 public class MusicPlayerDataGenerator {
 	
+	@SubscribeEvent
+	public static void data(GatherDataEvent event) {
+		final GenerationData data = new GenerationData(MusicPlayerMod.MODID, event);
+		if (event.includeClient()) {
+//			data.addProvider(MusicPlayerLanguagesProvider::new);
+		}
+	}
+	
 }
+
