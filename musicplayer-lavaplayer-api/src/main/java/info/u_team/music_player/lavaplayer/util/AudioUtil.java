30dd9499ec740df8dd35b2e0b233fa6df9cd7a73
HyCraftHD <hycrafthd@live.de>
2020-02-26 14:33:11 +0100

    Add audioutil

---
+++ musicplayer-lavaplayer-api/src/main/java/info/u_team/music_player/lavaplayer/util/AudioUtil.java
@@ -0,0 +1,40 @@
+package info.u_team.music_player.lavaplayer.util;
+
+import java.util.*;
+
+import javax.sound.sampled.*;
+
+public class AudioUtil {
+	
+	public static Mixer findMixer(String name, Line.Info lineInfo) {
+		Mixer defaultMixer = null;
+		for (Mixer.Info mixerInfo : AudioSystem.getMixerInfo()) {
+			final Mixer mixer = AudioSystem.getMixer(mixerInfo);
+			if (mixer.isLineSupported(lineInfo)) {
+				if (mixerInfo.getName().equals(name)) {
+					return mixer;
+				}
+				if (defaultMixer == null) {
+					defaultMixer = mixer;
+				}
+			}
+		}
+		return defaultMixer;
+	}
+	
+	public static List<String> findAudioDevices(Line.Info lineInfo) {
+		final List<String> list = new ArrayList<>();
+		for (Mixer.Info mixerInfo : AudioSystem.getMixerInfo()) {
+			final Mixer mixer = AudioSystem.getMixer(mixerInfo);
+			if (mixer.isLineSupported(lineInfo)) {
+				list.add(mixerInfo.getName());
+			}
+		}
+		return list;
+	}
+	
+	public static boolean hasLinesOpen(Mixer mixer) {
+		return mixer.getSourceLines().length != 0 || mixer.getTargetLines().length != 0;
+	}
+	
+}
