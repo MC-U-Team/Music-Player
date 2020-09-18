b24bf8a4ed1d64a03408ea93a74d0894e9b60817
HyCraftHD <hycrafthd@live.de>
2020-09-18 17:27:18 +0200

    Register the event handler

--- src/main/java/info/u_team/music_player/init/MusicPlayerClientConstruct.java
+++ src/main/java/info/u_team/music_player/init/MusicPlayerClientConstruct.java
@@ -3,6 +3,7 @@ package info.u_team.music_player.init;
 import info.u_team.music_player.MusicPlayerMod;
 import info.u_team.music_player.config.ClientConfig;
 import info.u_team.music_player.dependency.DependencyManager;
+import info.u_team.music_player.event.EventHandlerMusicPlayer;
 import info.u_team.music_player.musicplayer.MusicPlayerManager;
 import info.u_team.u_team_core.api.construct.*;
 import info.u_team.u_team_core.util.registry.BusRegister;
@@ -23,6 +24,8 @@ public class MusicPlayerClientConstruct implements IModConstruct {
 		MusicPlayerFiles.load();
 		
 		BusRegister.registerMod(MusicPlayerManager::registerMod);
+		
+		BusRegister.registerForge(EventHandlerMusicPlayer::registerForge);
 	}
 	
 }
