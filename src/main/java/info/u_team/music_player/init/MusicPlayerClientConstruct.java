b3e17795a6525112c6b6f7e7ff3bb71201de1093
HyCraftHD <hycrafthd@live.de>
2020-09-18 15:13:39 +0200

    Add music player client construct

---
+++ src/main/java/info/u_team/music_player/init/MusicPlayerClientConstruct.java
@@ -0,0 +1,14 @@
+package info.u_team.music_player.init;
+
+import info.u_team.music_player.MusicPlayerMod;
+import info.u_team.u_team_core.api.construct.*;
+
+@Construct(modid = MusicPlayerMod.MODID, client = true)
+public class MusicPlayerClientConstruct implements IModConstruct {
+	
+	@Override
+	public void construct() {
+		
+	}
+	
+}
