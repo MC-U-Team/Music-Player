354a306ce2103cd4e0e1a4eb81d77359c3360928
HyCraftHD <hycrafthd@live.de>
2020-09-18 15:11:35 +0200

    Remove old stuff from the mod main class

--- src/main/java/info/u_team/music_player/MusicPlayerMod.java
+++ src/main/java/info/u_team/music_player/MusicPlayerMod.java
@@ -1,34 +1,14 @@
 package info.u_team.music_player;
 
-import info.u_team.music_player.proxy.*;
-import info.u_team.u_team_core.api.IModProxy;
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.*;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(MusicPlayerMod.MODID)
 public class MusicPlayerMod {
 	
 	public static final String MODID = "musicplayer";
 	
-	private static final IModProxy PROXY = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> CommonProxy::new);
-	
 	public MusicPlayerMod() {
 		JarSignVerifier.checkSigned(MODID);
-		FMLJavaModLoadingContext.get().getModEventBus().register(this);
-		PROXY.construct();
 	}
-	
-	@SubscribeEvent
-	public void setup(FMLCommonSetupEvent event) {
-		PROXY.setup();
-	}
-	
-	public void ready(FMLLoadCompleteEvent event) {
-		PROXY.complete();
-	}
-	
 }
