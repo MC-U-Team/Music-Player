6ecc19a13668ebfe5fac85c500a641633ea3eff4
HyCraftHD <hycrafthd@live.de>
2020-09-19 17:05:00 +0200

    Add common construct

---
+++ src/main/java/info/u_team/music_player/init/MusicPlayerCommonConstruct.java
@@ -0,0 +1,13 @@
+package info.u_team.music_player.init;
+
+import info.u_team.music_player.MusicPlayerMod;
+import info.u_team.u_team_core.api.construct.*;
+
+@Construct(modid = MusicPlayerMod.MODID)
+public class MusicPlayerCommonConstruct implements IModConstruct {
+	
+	@Override
+	public void construct() {
+	}
+	
+}
