4695979da6f55e398a928bea8155f1f191fa6f98
HyCraftHD <hycrafthd@live.de>
2020-09-19 17:05:11 +0200

    Mark mod as client ony

--- src/main/java/info/u_team/music_player/init/MusicPlayerCommonConstruct.java
+++ src/main/java/info/u_team/music_player/init/MusicPlayerCommonConstruct.java
@@ -1,13 +1,18 @@
 package info.u_team.music_player.init;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import info.u_team.music_player.MusicPlayerMod;
 import info.u_team.u_team_core.api.construct.*;
+import net.minecraftforge.fml.*;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Construct(modid = MusicPlayerMod.MODID)
 public class MusicPlayerCommonConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 	}
 	
 }
