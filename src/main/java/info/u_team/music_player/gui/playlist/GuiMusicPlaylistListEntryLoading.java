891eeb48884f9896cef5922158e71d7b30fb5410
HyCraftHD <hycrafthd@live.de>
2020-09-19 13:06:57 +0200

    Fix the music playlist entry loading

--- src/main/java/info/u_team/music_player/gui/playlist/GuiMusicPlaylistListEntryLoading.java
+++ src/main/java/info/u_team/music_player/gui/playlist/GuiMusicPlaylistListEntryLoading.java
@@ -2,12 +2,14 @@ package info.u_team.music_player.gui.playlist;
 
 import static info.u_team.music_player.init.MusicPlayerLocalization.*;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+
 public class GuiMusicPlaylistListEntryLoading extends GuiMusicPlaylistListEntry {
 	
 	@Override
-	public void render(int slotIndex, int entryY, int entryX, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float partialTicks) {
+	public void render(MatrixStack matrixStack, int slotIndex, int entryY, int entryX, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float partialTicks) {
 		final String text = getTranslation(GUI_PLAYLIST_LOADING);
-		minecraft.fontRenderer.drawString(text, entryX + (entryWidth / 2) - (minecraft.fontRenderer.getStringWidth(text) / 2), entryY + 20, 0xFF0000);
+		minecraft.fontRenderer.drawString(matrixStack, text, entryX + (entryWidth / 2) - (minecraft.fontRenderer.getStringWidth(text) / 2), entryY + 20, 0xFF0000);
 	}
 	
 }
