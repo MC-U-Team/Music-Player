50b7bb782bb359986a89cdcdfeb7a79b24196e76
HyCraftHD <hycrafthd@live.de>
2020-10-16 17:33:29 +0200

    Remove unused methods

--- src/main/java/info/u_team/music_player/dependency/classloader/DependencyClassLoader.java
+++ src/main/java/info/u_team/music_player/dependency/classloader/DependencyClassLoader.java
@@ -1,13 +1,8 @@
 package info.u_team.music_player.dependency.classloader;
 
 import java.net.*;
-import java.nio.file.Path;
-
-import org.apache.logging.log4j.*;
 
 public class DependencyClassLoader extends URLClassLoader {
-	
-	private static final Logger LOGGER = LogManager.getLogger();
 	
 	static {
 		ClassLoader.registerAsParallelCapable();
@@ -32,13 +27,5 @@ public class DependencyClassLoader extends URLClassLoader {
 	@Override
 	public void addURL(URL url) {
 		super.addURL(url);
-	}
-	
-	public void addPath(Path path) {
-		try {
-			addURL(path.toUri().toURL());
-		} catch (final MalformedURLException ex) {
-			LOGGER.error("Could not add dependency path to classloader", ex);
-		}
 	}
 }
