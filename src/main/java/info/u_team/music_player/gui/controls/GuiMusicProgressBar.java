eaf7833ce3d100ffcc489af13e57b0f345cab108
HyCraftHD <hycrafthd@live.de>
2021-02-15 01:58:43 +0100

    Update gui music progress bar (wip as x,y may not be right at first init)

--- src/main/java/info/u_team/music_player/gui/controls/GuiMusicProgressBar.java
+++ src/main/java/info/u_team/music_player/gui/controls/GuiMusicProgressBar.java
@@ -6,32 +6,46 @@ import info.u_team.music_player.gui.util.GuiTrackUtils;
 import info.u_team.music_player.lavaplayer.api.audio.IAudioTrack;
 import info.u_team.music_player.lavaplayer.api.queue.ITrackManager;
 import info.u_team.u_team_core.gui.elements.ProgressBar;
-import info.u_team.u_team_core.gui.render.ScalingTextRender;
+import info.u_team.u_team_core.gui.renderer.ScalingTextRenderer;
+import info.u_team.u_team_core.util.RGBA;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 
 public class GuiMusicProgressBar extends ProgressBar {
 	
-	private final ScalingTextRender positionRender;
-	private final ScalingTextRender durationRender;
+	private static final RGBA GREY = new RGBA(0x555555FF);
+	private static final RGBA GREEN = new RGBA(0x3E9100FF);
+	
+	private static final RGBA YELLOW = new RGBA(0xFFFF00FF);
+	
+	private final ScalingTextRenderer positionRender;
+	private final ScalingTextRenderer durationRender;
 	
 	public GuiMusicProgressBar(ITrackManager manager, int x, int y, int width, int height, float scale) {
-		super(x, y, width, height, 0xFF555555, 0xFF3e9100, () -> getProgress(manager), (value) -> updateProgress(manager, value));
+		super(x, y, width, height, GREY, GREEN, () -> getProgress(manager), (value) -> updateProgress(manager, value));
 		final FontRenderer fontRender = Minecraft.getInstance().fontRenderer;
-		positionRender = new ScalingTextRender(() -> fontRender, () -> GuiTrackUtils.getFormattedPosition(manager.getCurrentTrack()));
+		positionRender = new ScalingTextRenderer(() -> fontRender, () -> GuiTrackUtils.getFormattedPosition(manager.getCurrentTrack()), x, y);
 		positionRender.setScale(scale);
-		positionRender.setColor(0xFFFF00);
-		durationRender = new ScalingTextRender(() -> fontRender, () -> GuiTrackUtils.getFormattedDuration(manager.getCurrentTrack()));
+		positionRender.setColor(YELLOW);
+		positionRender.setTextChanged(renderer -> {
+			renderer.setX(getX() - renderer.getTextWidth() - (renderer.getScale() < 1 ? 3 : 5));
+			renderer.setY(getY() - (renderer.getScale() < 1 ? 1 : 2));
+		});
+		durationRender = new ScalingTextRenderer(() -> fontRender, () -> GuiTrackUtils.getFormattedDuration(manager.getCurrentTrack()), x, y);
 		durationRender.setScale(scale);
-		durationRender.setColor(0xFFFF00);
+		durationRender.setColor(YELLOW);
+		durationRender.setTextChanged(renderer -> {
+			renderer.setX(getX() + getWidth() + (renderer.getScale() < 1 ? 3 : 5));
+			renderer.setY(getY() - (renderer.getScale() < 1 ? 1 : 2));
+		});
 	}
 	
 	@Override
 	public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
 		super.render(matrixStack, mouseX, mouseY, partialTicks);
 		
-		positionRender.draw(getX() - positionRender.getTextWidth() - (positionRender.getScale() < 1 ? 3 : 5), getY() - (positionRender.getScale() < 1 ? 1 : 2));
-		durationRender.draw(getX() + getWidth() + (positionRender.getScale() < 1 ? 3 : 5), getY() - (positionRender.getScale() < 1 ? 1 : 2));
+		positionRender.render(matrixStack, mouseX, mouseY, partialTicks);
+		durationRender.render(matrixStack, mouseX, mouseY, partialTicks);
 	}
 	
 	private static double getProgress(ITrackManager manager) {
