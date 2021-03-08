e1933bfe15575744fef511fc8cea60d26a6e0420
HyCraftHD <hycrafthd@live.de>
2021-03-08 03:49:42 +0100

    Add final

--- musicplayer-lavaplayer-api/src/main/java/info/u_team/music_player/lavaplayer/api/util/AudioUtil.java
+++ musicplayer-lavaplayer-api/src/main/java/info/u_team/music_player/lavaplayer/api/util/AudioUtil.java
@@ -8,7 +8,7 @@ public class AudioUtil {
 	
 	public static List<String> findAudioDevices(Line.Info lineInfo) {
 		final List<String> list = new ArrayList<>();
-		for (Mixer.Info mixerInfo : AudioSystem.getMixerInfo()) {
+		for (final Mixer.Info mixerInfo : AudioSystem.getMixerInfo()) {
 			final Mixer mixer = AudioSystem.getMixer(mixerInfo);
 			if (mixer.isLineSupported(lineInfo)) {
 				list.add(mixerInfo.getName());
