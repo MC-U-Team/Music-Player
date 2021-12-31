8faa8154681a239d8e2957e5208a9b853da59eb0
HyCraftHD <hycrafthd@live.de>
2021-12-31 04:27:55 +0100

    Add hack to add jarlookup system back (former modjar)

--- src/main/java/info/u_team/music_player/dependency/DependencyManager.java
+++ src/main/java/info/u_team/music_player/dependency/DependencyManager.java
@@ -1,11 +1,14 @@
 package info.u_team.music_player.dependency;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -15,10 +18,14 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
 
+import cpw.mods.cl.ModularURLHandler;
+import cpw.mods.cl.ModularURLHandler.IURLProvider;
 import cpw.mods.modlauncher.api.LamdbaExceptionUtils;
 import info.u_team.music_player.MusicPlayerMod;
 import info.u_team.music_player.dependency.classloader.DependencyClassLoader;
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.fml.unsafe.UnsafeHacks;
 import net.minecraftforge.forgespi.locating.IModFile;
 
 public class DependencyManager {
@@ -33,6 +40,8 @@ public class DependencyManager {
 	
 	public static void load() {
 		LOGGER.info(MARKER_LOAD, "Load dependencies");
+		
+		setupURLStreamHack();
 		
 		final String devPath = System.getProperty("musicplayer.dev");
 		if (devPath != null) {
@@ -67,7 +76,7 @@ public class DependencyManager {
 	}
 	
 	private static URL createInternalURL(Path path) {
-		final String url = "modjar://" + MusicPlayerMod.MODID + path;
+		final String url = "jarlookup://" + MusicPlayerMod.MODID + path;
 		LOGGER.debug(MARKER_LOAD, "Create mod jar url ({}) from path ({}).", url, path);
 		try {
 			return new URL(url);
@@ -82,5 +91,38 @@ public class DependencyManager {
 	private static void addToMusicPlayerDependencies(URL url) {
 		MUSICPLAYER_CLASSLOADER.addURL(url);
 		LOGGER.info(MARKER_ADD, "Added new jar file ({}) to the musicplayer dependency classloader.", url); // TODO change to debug
+	}
+	
+	// TODO replace this with a less hacky solution
+	private static void setupURLStreamHack() {
+		LOGGER.info(MARKER_ADD, "Trying to initialize url stream handler for musicplayer url protocol.");
+		
+		try {
+			final var field = ModularURLHandler.class.getDeclaredField("handlers");
+			final Map<String, IURLProvider> handlers = UnsafeHacks.getField(field, ModularURLHandler.INSTANCE);
+			
+			handlers.put("jarlookup", new IURLProvider() {
+				
+				@Override
+				public String protocol() {
+					return "jarlookup";
+				}
+				
+				@Override
+				public Function<URL, InputStream> inputStreamFunction() {
+					return url -> {
+						try {
+							final var path = FMLLoader.getLoadingModList().getModFileById(url.getHost()).getFile().findResource(url.getPath().substring(1));
+							return Files.newInputStream(path);
+						} catch (IOException ex) {
+							LOGGER.error("Could not find resource {}", url);
+							throw new UncheckedIOException(ex);
+						}
+					};
+				}
+			});
+		} catch (Throwable ex) {
+			throw new RuntimeException("Could not setup url stream handler. Please report to mod author.", ex);
+		}
 	}
 }
