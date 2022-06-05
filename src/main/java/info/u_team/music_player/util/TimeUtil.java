09d7d942862fac267ca407354fdcdaff1ed66be7
HyCraftHD <hycrafthd@live.de>
2022-06-05 15:28:57 +0200

    Cleanup Timeutil a bit

--- src/main/java/info/u_team/music_player/util/TimeUtil.java
+++ src/main/java/info/u_team/music_player/util/TimeUtil.java
@@ -2,11 +2,10 @@ package info.u_team.music_player.util;
 
 public class TimeUtil {
 	
+	private final static int MINUTES_IN_AN_HOUR = 60;
+	private final static int SECONDS_IN_A_MINUTE = 60;
+	
 	public static String timeConversion(long seconds) {
-		
-		final int MINUTES_IN_AN_HOUR = 60;
-		final int SECONDS_IN_A_MINUTE = 60;
-		
 		long minutes = seconds / SECONDS_IN_A_MINUTE;
 		seconds -= minutes * SECONDS_IN_A_MINUTE;
 		
