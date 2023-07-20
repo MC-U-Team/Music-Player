b221d1b35d67ba466117f7b99a996d44ad8f5e13
HyCraftHD <hycrafthd@live.de>
2023-07-21 01:25:21 +0200

    Fixes #666, #806, #809, #810. Possibly fixes #808

--- musicplayer-lavaplayer/src/main/java/info/u_team/music_player/lavaplayer/sources/AudioSources.java
+++ musicplayer-lavaplayer/src/main/java/info/u_team/music_player/lavaplayer/sources/AudioSources.java
@@ -1,8 +1,13 @@
 package info.u_team.music_player.lavaplayer.sources;
 
+import java.util.function.Supplier;
+
+import org.slf4j.LoggerFactory;
+
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.bandcamp.BandcampAudioSourceManager;
-import com.sedmelluq.discord.lavaplayer.source.beam.BeamAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.getyarn.GetyarnAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.http.HttpAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.local.LocalAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.soundcloud.SoundCloudAudioSourceManager;
@@ -13,15 +18,25 @@ import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager
 public class AudioSources {
 	
 	public static void registerSources(AudioPlayerManager audioPlayerManager) {
-		final YoutubeAudioSourceManager youtube = new YoutubeAudioSourceManager(true, System.getProperty("musicplayer.lavaplayer.youtube.email"), System.getProperty("musicplayer.lavaplayer.youtube.password"));
-		youtube.setPlaylistPageCount(100);
-		audioPlayerManager.registerSourceManager(youtube);
-		audioPlayerManager.registerSourceManager(SoundCloudAudioSourceManager.createDefault());
-		audioPlayerManager.registerSourceManager(new BandcampAudioSourceManager());
-		audioPlayerManager.registerSourceManager(new VimeoAudioSourceManager());
-		audioPlayerManager.registerSourceManager(new TwitchStreamAudioSourceManager());
-		audioPlayerManager.registerSourceManager(new BeamAudioSourceManager());
-		audioPlayerManager.registerSourceManager(new HttpAudioSourceManager());
-		audioPlayerManager.registerSourceManager(new LocalAudioSourceManager());
+		registerManager(audioPlayerManager, () -> {
+			final YoutubeAudioSourceManager youtube = new YoutubeAudioSourceManager(true, System.getProperty("musicplayer.lavaplayer.youtube.email"), System.getProperty("musicplayer.lavaplayer.youtube.password"));
+			youtube.setPlaylistPageCount(100);
+			return youtube;
+		});
+		registerManager(audioPlayerManager, SoundCloudAudioSourceManager::createDefault);
+		registerManager(audioPlayerManager, BandcampAudioSourceManager::new);
+		registerManager(audioPlayerManager, VimeoAudioSourceManager::new);
+		registerManager(audioPlayerManager, TwitchStreamAudioSourceManager::new);
+		registerManager(audioPlayerManager, GetyarnAudioSourceManager::new);
+		registerManager(audioPlayerManager, HttpAudioSourceManager::new);
+		registerManager(audioPlayerManager, LocalAudioSourceManager::new);
+	}
+	
+	private static void registerManager(AudioPlayerManager audioPlayerManager, Supplier<AudioSourceManager> audioSourceManager) {
+		try {
+			audioPlayerManager.registerSourceManager(audioSourceManager.get());
+		} catch (final Exception ex) {
+			LoggerFactory.getLogger(AudioSources.class).warn("Cannot register audio source. Some music tracks might not be playable. Most often it is caused by an unstable internet connection or blocked services", ex);
+		}
 	}
 }
