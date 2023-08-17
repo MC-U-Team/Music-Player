1805c4f40da51e8fe6fba1ce0d681bd1bb58c2ad
HyCraftHD <hycrafthd@live.de>
2023-08-17 14:04:46 +0200

    Update testing main ground

--- musicplayer-lavaplayer/src/test_player/java/info/u_team/music_player/lavaplayer/testimpl/Main.java
+++ musicplayer-lavaplayer/src/test_player/java/info/u_team/music_player/lavaplayer/testimpl/Main.java
@@ -1,15 +1,20 @@
 package info.u_team.music_player.lavaplayer.testimpl;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import info.u_team.music_player.lavaplayer.MusicPlayer;
-import info.u_team.music_player.lavaplayer.api.IMusicPlayer;
 import info.u_team.music_player.lavaplayer.api.audio.IAudioTrack;
 import info.u_team.music_player.lavaplayer.api.queue.ITrackManager;
 import info.u_team.music_player.lavaplayer.api.queue.ITrackQueue;
@@ -23,7 +28,7 @@ public class Main {
 	}
 	
 	public Main() {
-		final IMusicPlayer musicPlayer = new MusicPlayer();
+		final MusicPlayer musicPlayer = new MusicPlayer();
 		musicPlayer.startAudioOutput();
 		
 		musicPlayer.setVolume(10);
@@ -50,8 +55,12 @@ public class Main {
 					manager.setPaused(false);
 				} else if (line.startsWith("skip")) {
 					manager.skip();
+				} else if (line.startsWith("rate")) {
+					final IAudioTrack audioTrack = manager.getCurrentTrack();
+					System.out.println((float) audioTrack.getPosition() / audioTrack.getDuration());
 				} else if (line.startsWith("stop")) {
 					manager.stop();
+					System.exit(0);
 				} else if (line.startsWith("setending")) {
 					final IAudioTrack audioTrack = manager.getCurrentTrack();
 					audioTrack.setPosition(audioTrack.getDuration() - 10000);
@@ -63,6 +72,14 @@ public class Main {
 			scanner.close();
 		}, "Music player manager").start();
 		
+		final AtomicInteger counter = new AtomicInteger();
+		final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss").withZone(ZoneId.systemDefault());
+		Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
+			final IAudioTrack audioTrack = manager.getCurrentTrack();
+			if (audioTrack != null) {
+				System.out.println(counter.getAndIncrement() + ": " + formatter.format(Instant.now()) + " -> Rate: " + String.format("%.2f%%", ((float) (audioTrack.getPosition()) / audioTrack.getDuration()) * 100));
+			}
+		}, 1, 10, TimeUnit.SECONDS);
 	}
 	
 	private class TrackQueue implements ITrackQueue {
