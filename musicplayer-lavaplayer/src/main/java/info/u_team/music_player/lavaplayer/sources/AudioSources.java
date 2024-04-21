6907692cdbb4e7bc1a88a0918e3b39d8291ee375
HyCraftHD <hycrafthd@live.de>
2024-04-21 22:35:43 +0200

    Update lavaplayer and gradle files

--- musicplayer-lavaplayer/src/main/java/info/u_team/music_player/lavaplayer/sources/AudioSources.java
+++ musicplayer-lavaplayer/src/main/java/info/u_team/music_player/lavaplayer/sources/AudioSources.java
@@ -2,6 +2,7 @@ package info.u_team.music_player.lavaplayer.sources;
 
 import java.util.function.Supplier;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
@@ -10,12 +11,16 @@ import com.sedmelluq.discord.lavaplayer.source.bandcamp.BandcampAudioSourceManag
 import com.sedmelluq.discord.lavaplayer.source.getyarn.GetyarnAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.http.HttpAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.local.LocalAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.nico.NicoAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.soundcloud.SoundCloudAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.twitch.TwitchStreamAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.vimeo.VimeoAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.yamusic.YandexMusicAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
 
 public class AudioSources {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(AudioSources.class);
 	
 	public static void registerSources(AudioPlayerManager audioPlayerManager) {
 		registerManager(audioPlayerManager, () -> {
@@ -28,15 +33,21 @@ public class AudioSources {
 		registerManager(audioPlayerManager, VimeoAudioSourceManager::new);
 		registerManager(audioPlayerManager, TwitchStreamAudioSourceManager::new);
 		registerManager(audioPlayerManager, GetyarnAudioSourceManager::new);
+		registerManager(audioPlayerManager, NicoAudioSourceManager::new);
+		registerManager(audioPlayerManager, YandexMusicAudioSourceManager::new);
 		registerManager(audioPlayerManager, HttpAudioSourceManager::new);
 		registerManager(audioPlayerManager, LocalAudioSourceManager::new);
 	}
 	
 	private static void registerManager(AudioPlayerManager audioPlayerManager, Supplier<AudioSourceManager> audioSourceManager) {
 		try {
-			audioPlayerManager.registerSourceManager(audioSourceManager.get());
+			final AudioSourceManager manager = audioSourceManager.get();
+			if (manager != null) {
+				LOGGER.info("Register {} source manager for music player", manager.getSourceName());
+				audioPlayerManager.registerSourceManager(manager);
+			}
 		} catch (final Exception ex) {
-			LoggerFactory.getLogger(AudioSources.class).warn("Cannot register audio source. Some music tracks might not be playable. Most often it is caused by an unstable internet connection or blocked services", ex);
+			LOGGER.warn("Cannot register source manager. Some music tracks might not be playable. Most often it is caused by an unstable internet connection or blocked services", ex);
 		}
 	}
 }
